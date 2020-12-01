775f2cf8c8dc45bbe1d17b9d4ae01801e32896d9
jgarin <garastovych@gmail.com>
2020-12-01 12:37:58 +0100

    Fixed multiple `loaddata` calls on invalidation

--- repository/src/main/java/com/jgarin/composecalculator/repository/InMemoryDataRepository.kt
+++ repository/src/main/java/com/jgarin/composecalculator/repository/InMemoryDataRepository.kt
@@ -1,15 +1,22 @@
 package com.jgarin.composecalculator.repository
 
 import com.jgarin.composecalculator.models.DurationDomain
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.withContext
 
 internal class InMemoryDataRepository : DataRepository {
 
     private val data = ArrayList<DurationDomain>()
-    private val invalidateAction = MutableSharedFlow<Unit>(replay = 1)
+    private val invalidateAction = MutableSharedFlow<Unit>()
+    private val scope = CoroutineScope(Dispatchers.Unconfined + SupervisorJob())
+    private val items = invalidateAction.onSubscription {
+        val count = invalidateAction.subscriptionCount.value
+        if (count == 1) {
+            emit(Unit)
+        }
+    }.map { loadData() }
+        .shareIn(scope, replay = 1, started = SharingStarted.WhileSubscribed())
+
 
     override suspend fun creteItem(hours: Int, minutes: Int) = withContext(Dispatchers.IO) {
         delay(300) // emulate backend delay
@@ -22,13 +29,7 @@ internal class InMemoryDataRepository : DataRepository {
         invalidate()
     }
 
-    override suspend fun readItems(): Flow<List<DurationDomain>> = invalidateAction
-        .onSubscription {
-            val count = invalidateAction.subscriptionCount.value
-            if (count == 1) {
-                emit(Unit)
-            }
-        }.map { loadData() }
+    override suspend fun readItems(): Flow<List<DurationDomain>> = items
 
     override suspend fun updateItem(item: DurationDomain) = withContext(Dispatchers.IO) {
         delay(500) // emulate backend delay
